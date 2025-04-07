19911784d00af2b2bae47275824afd2bb4318591
LG <LG@DESKTOP-G8J5AFN>
2025-04-07 15:39:18 -0500

    inital commit

---
+++ Instructor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class Instructor
+{
+    public int InstructorId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Username { get; set; } // Note: In a real-world scenario, you'd want to hash this
+}
--- obj/Debug/net8.0-ios/iossimulator-x64/BlazorDesktop.AssemblyInfo.cs
+++ obj/Debug/net8.0-ios/iossimulator-x64/BlazorDesktop.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorDesktop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+0f5c507e011a7c422698213d036e3b19e581ca35")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorDesktop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorDesktop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
